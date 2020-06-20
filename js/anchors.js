22aac0b93ccd8b5854b3141bc52f7e75cec7bd77
Nikolay Miroshnichenko <asohar29@gmail.com>
2020-06-20 12:31:45 +0300

    last1231

--- js/anchors.js
+++ js/anchors.js
@@ -1,14 +1,14 @@
-const anchors = document.querySelectorAll('.anchors')
+const anchors = document.querySelectorAll( '.anchors' )
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
+for ( let anchor of anchors ) {
+  anchor.addEventListener( 'click', function ( e ) {
     e.preventDefault();
-    
-    const blockID = anchor.getAttribute('href').substr(1);
-    
-    document.getElementById(blockID).scrollIntoView({
+
+    const blockID = anchor.getAttribute( 'href' ).substr( 1 );
+
+    document.getElementById( blockID ).scrollIntoView( {
       behavior: 'smooth',
       block: 'start'
-    })
-  })
+    } )
+  } )
 }
